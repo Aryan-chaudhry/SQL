dd97d8b9fc635d87df5462c901ff2bb36dab7dc4
Aryan <Aryanchaudhary2629@gmail.com>
2025-05-18 13:55:22 +0530

    Updated Code

--- PracticeProblem/50Problems.sql
+++ PracticeProblem/50Problems.sql
@@ -32,3 +32,56 @@ SELECT LTRIM(Department) FROM Worker;
 
 -- Q-8. Write an SQL query that fetches the unique values of DEPARTMENT from Worker table and prints its length.
 SELECT DISTINCT(department), LENGTH(department) FROM Worker;
+
+-- Q-9.Write an SQL query to print the FIRST_NAME from Worker table after replacing ‘a’ with ‘A’.
+SELECT REPLACE(first_name, 'a','A') FROM Worker;
+
+-- Q-10. Write an SQL query to print the FIRST_NAME and LAST_NAME from Worker table into a single column COMPLETE_NAME.
+SELECT CONCAT(first_name, ' ', last_name) AS COMPLETE_NAME FROM Worker;
+
+-- Q-11. Write an SQL query to print all Worker details from the Worker table order by FIRST_NAME Ascending.
+SELECT * FROM WORKER
+ORDER BY first_name ASC;
+
+-- Q-12. Write an SQL query to print all Worker details from the Worker table order by 
+-- FIRST_NAME Ascending and DEPARTMENT Descending.
+SELECT * FROM WORKER
+ORDER BY First_name ASC, department DESC;
+
+-- Q-13. Write an SQL query to print details for Workers with the first name as “Vipul” and “Satish” from Worker table.
+SELECT * FROM WORKER
+WHERE first_name IN ('Vipul', 'Satish');    
+
+-- Q-14. Write an SQL query to print details of workers excluding first names, “Vipul” and “Satish” from Worker table.
+SELECT * FROM Worker
+WHERE first_name NOT IN('Vipul', 'Satish');
+
+-- Q-15. Write an SQL query to print details of Workers with DEPARTMENT name as “Admin*”.
+SELECT * FROM Worker
+WHERE Department like 'ADMIN%';
+
+-- Q-16. Write an SQL query to print details of the Workers whose FIRST_NAME contains ‘a’.
+SELECT * FROM Worker
+WHERE First_name LIKE '%a%';
+
+-- Q-17. Write an SQL query to print details of the Workers whose FIRST_NAME ends with ‘a’.
+SELECT * from Worker
+WHERE First_name LIKE '%a';
+
+-- Q-18. Write an SQL query to print details of the Workers whose FIRST_NAME ends with ‘h’ and contains six alphabets.
+SELECT * from Worker
+WHERE first_name LIKE '%h' AND LENGTH(first_name) = 6
+
+--OR
+
+SELECT * from Worker
+WHERE first_name LIKE '_____h';
+
+-- Q-19. Write an SQL query to print details of the Workers whose SALARY lies between 100000 and 500000.
+SELECT * FROM Worker
+WHERE SALARY BETWEEN 100000 AND 500000
+
+-- Q-20. Write an SQL query to print details of the Workers who have joined in Feb’2014.
+SELECT * FROM Worker
+WHERE YEAR(joining_date) = 2014 AND MONTH(joining_date) = 02;
+
